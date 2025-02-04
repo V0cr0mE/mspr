694b33d5da6bf97ea1bc02f84347893afc53c580
SEGHIR <anesseghir2001@gmail.com>
2025-02-04 23:24:44 +0100

    style

--- app.py
+++ app.py
@@ -13,14 +13,11 @@ from routes.pandemic import bp as pandemic_bp
 from routes.pandemic_country import bp as pandemic_country_bp
 from routes.daily_pandemic_country import bp as daily_pandemic_country_bp
 
-
 sys.path.append(os.path.join(os.path.dirname(__file__), 'load'))
 sys.path.append(os.path.join(os.path.dirname(__file__), 'etl'))
 
-
 def create_app():
     app = Flask(__name__)
-
 
     app.register_blueprint(continent_bp)
     app.register_blueprint(country_bp)
@@ -28,32 +25,70 @@ def create_app():
     app.register_blueprint(pandemic_country_bp)
     app.register_blueprint(daily_pandemic_country_bp)
 
-    
     dash_app = dash.Dash(__name__, server=app, url_base_pathname='/')
 
     dash_app.layout = html.Div([
-        html.H1("Statistiques Pandemic", style={'textAlign': 'center', 'color': 'black'}),
 
         
-        dcc.Dropdown(id='country-dropdown', options=[], value=None, style={'width': '50%', 'margin': '20px auto'}),
-        dcc.Dropdown(id='pandemic-dropdown', options=[], value=None, style={'width': '50%', 'margin': '20px auto'}),
+        html.Nav([html.H2("Pandemic Statistics Dashboard", style={'color': 'white', 'margin': '0', 'padding': '10px', 'textAlign': 'center'})],
+                 style={'backgroundColor': 'rgba(0, 0, 0, 0)', 'padding': '10px'}),
 
-        dcc.DatePickerRange(
-            id='date-picker-range',
-            start_date="2020-01-01",
-            end_date="2025-01-01",   
-            display_format='YYYY-MM-DD',
-            style={'width': '50%', 'margin': '20px auto'}
-        ),
+        
+        html.Div([
+            
+            html.Div([
+                html.Label("Select Country", style={'fontSize': '18px', 'fontWeight': 'bold','color': 'white'}),
+                dcc.Dropdown(id='country-dropdown', options=[], value=None,
+                             style={'width': '100%', 'fontFamily': 'Arial, sans-serif', 'borderRadius': '8px', 'border': '1px solid #ddd'}),
+            ], style={'width': '30%', 'padding': '10px'}),
 
-        # Conteneur des cartes
-        html.Div(id='cards-container', style={'display': 'flex', 'justifyContent': 'center', 'flexWrap': 'wrap', 'gap': '20px'}),
+            
+            html.Div([
+                html.Label("Select Pandemic", style={'fontSize': '18px', 'fontWeight': 'bold','color': 'white'}),
+                dcc.Dropdown(id='pandemic-dropdown', options=[], value=None,
+                             style={'width': '100%', 'fontFamily': 'Arial, sans-serif', 'borderRadius': '8px', 'border': '1px solid #ddd'}),
+            ], style={'width': '30%', 'padding': '10px'}),
 
-        # Graphique d'évolution des guérisons
-        dcc.Graph(id='recovery-trend', style={'width': '80%', 'margin': '20px auto'})
-    ], style={'backgroundColor': '#f0f0f0', 'minHeight': '100vh'})
+           
+            html.Div([
+                html.Label("Select Date Range", style={'fontSize': '18px', 'fontWeight': 'bold','color': 'white'}),
+                dcc.DatePickerRange(
+                    id='date-picker-range',
+                    start_date="2020-01-01",
+                    end_date="2025-01-01",
+                    display_format='YYYY-MM-DD',
+                    style={
+                        'width': '100%',
+                        'fontFamily': 'Arial, sans-serif',
+                        'borderRadius': '8px',
+                        'border': '1px solid #ddd',
+                        'backgroundColor': '#2c3e50',
+                        'boxShadow': '0 1px 3px rgba(0, 0, 0, 0.12)',
+                        'fontSize': '16px',
+                        'display': 'flex',
+                        'justifyContent': 'space-between',
+                    },
+                    calendar_orientation='vertical',
+                ),
+            ], ),
 
+        ], style={'display': 'flex', 'justifyContent': 'space-between', 'padding': '20px', 'backgroundColor': 'rgba(0, 0, 0, 0)', 'boxShadow': '0 4px 6px rgba(0, 0, 0, 0.1)'}),
 
+        
+        html.Div(id='cards-container', style={'display': 'flex', 'flexWrap': 'wrap', 'gap': '20px', 'justifyContent': 'center', 'margin': '20px auto'}),
+
+       
+        dcc.Graph(id='recovery-trend', style={'width': '50%', 'margin': '40px auto','backgroundColor': 'rgba(0, 0, 0, 0)'})
+    ], style={
+        'backgroundImage': 'url(https://mediclinic.scene7.com/is/image/mediclinic/hirslanden-corona-virus-teaser:1-1?_ck=1616227095797&wid=1050&hei=1050&dpr=off)',
+        'backgroundSize': 'cover',
+        'backgroundPosition': 'center center',
+        'minHeight': '100vh',
+        'fontFamily': 'Arial, sans-serif',
+        'backgroundColor': 'rgba(0, 0, 0, 0)'
+    })
+
+   
     def get_countries():
         response = requests.get('http://127.0.0.1:5000/country')
         return response.json() if response.status_code == 200 else []
@@ -66,6 +101,7 @@ def create_app():
         response = requests.get(f'http://127.0.0.1:5000/daily_pandemic_country/{id_country}/{id_pandemic}')
         return response.json() if response.status_code == 200 else []
 
+   
     @dash_app.callback(
         Output('country-dropdown', 'options'),
         Input('country-dropdown', 'value')
@@ -82,7 +118,6 @@ def create_app():
         pandemics = get_pandemics()
         return [{'label': pandemic['name'], 'value': pandemic['id_pandemic']} for pandemic in pandemics]
 
-
     @dash_app.callback(
         Output('cards-container', 'children'),
         [Input('country-dropdown', 'value'),
@@ -90,18 +125,18 @@ def create_app():
     )
     def update_cards(country_id, pandemic_id):
         if not country_id or not pandemic_id:
-            return html.P("Veuillez sélectionner un pays et une pandémie.", style={'textAlign': 'center', 'color': 'black'})
+            return html.P("Veuillez sélectionner un pays et une pandémie.", style={'textAlign': 'center', 'color': 'white'})
 
         response = requests.get(f'http://127.0.0.1:5000/pandemic_country/{country_id}/{pandemic_id}')
         if response.status_code == 200:
             data = response.json()
             cards = [
                 html.Div([html.H3("Total Deaths"), html.P(f"{data.get('total_deaths', 0)}")],
-                         style={'border': '1px solid #d04e47', 'padding': '10px', 'width': '200px', 'backgroundColor': '#b94a3b', 'borderRadius': '10px', 'textAlign': 'center', 'color': 'white'}),
-                html.Div([html.H3("Total Confirmed Cases"), html.P(f"{data.get('total_confirmed', 0)}")],
-                         style={'border': '1px solid #e67e22', 'padding': '10px', 'width': '200px', 'backgroundColor': '#e17e22', 'borderRadius': '10px', 'textAlign': 'center', 'color': 'white'}),
+                         style={'border': '1px solid #d04e47', 'padding': '10px', 'width': '200px', 'backgroundColor': 'rgba(0, 0, 0, 0)','backdrop-filter': 'blur(50px)', 'borderRadius': '10px', 'textAlign': 'center', 'color': '#e67e22','font-weight': 'bold','font-size': '20px'}),
+                html.Div([html.H3("Total Cases"), html.P(f"{data.get('total_confirmed', 0)}")],
+                         style={'border': '1px solid #e67e22', 'padding': '10px', 'width': '200px', 'backgroundColor': 'rgba(0, 0, 0, 0)','backdrop-filter': 'blur(50px)', 'borderRadius': '10px', 'textAlign': 'center', 'color': '#5b8fd4','font-weight': 'bold','font-size': '20px'}),
                 html.Div([html.H3("Total Recovered"), html.P(f"{data.get('total_recovered', 0)}")],
-                         style={'border': '1px solid #2ecc71', 'padding': '10px', 'width': '200px', 'backgroundColor': '#229f59', 'borderRadius': '10px', 'textAlign': 'center', 'color': 'white'})
+                         style={'border': '1px solid #2ecc71', 'padding': '10px', 'width': '200px', 'backgroundColor': 'rgba(0, 0, 0, 0)','backdrop-filter': 'blur(50px)','borderRadius': '10px', 'textAlign': 'center', 'color': '#27ae60','font-size': '20px','font-weight': 'bold'})
             ]
             return cards
         return html.P("Données non trouvées.", style={'textAlign': 'center', 'color': 'black'})
@@ -124,14 +159,12 @@ def create_app():
         df = pd.DataFrame(data)
         df['date'] = pd.to_datetime(df['date'])
 
- 
         df_filtered = df[(df['date'] >= start_date) & (df['date'] <= end_date)]
 
         if 'daily_new_cases' not in df_filtered.columns:
             return px.line(title="Données manquantes")
 
-        
-        fig = px.line(df_filtered, x='date', y='daily_new_cases', 
+        fig = px.line(df_filtered, x='date', y='daily_new_cases',
                       title="Évolution du Nombre de Cas Quotidiens",
                       labels={'daily_new_cases': 'Cas quotidiens', 'date': 'Date'},
                       markers=True)
