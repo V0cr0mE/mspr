1b6084745cd1f4c0760dcc3d2f41d683a8fadc09
SEGHIR <anesseghir2001@gmail.com>
2025-04-10 12:13:10 +0200

    realisation de la documentation swagger

--- routes/continent.py
+++ routes/continent.py
@@ -9,6 +9,8 @@ def get_continents():
     """
     Récupérer tous les continents
     ---
+    tags:
+      - Continent
     responses:
       200:
         description: Liste des continents
@@ -28,6 +30,24 @@ def get_continents():
 # Route pour ajouter un continent
 @bp.route('', methods=['POST'])
 def add_new_continent():
+    """
+    Ajouter un nouveau continent
+    ---
+    tags:
+      - Continent
+    parameters:
+      - in: body
+        name: continent
+        required: true
+        schema:
+          type: object
+          properties:
+            continent:
+              type: string
+    responses:
+      201:
+        description: Continent ajouté avec succès
+    """
     data = request.get_json()
     continent_name = data.get('continent')
     add_continent(continent_name)
@@ -36,18 +56,52 @@ def add_new_continent():
 # Route pour supprimer un continent
 @bp.route('/<int:id>', methods=['DELETE'])
 def delete_continent_route(id):
+    """
+    Supprimer un continent
+    ---
+    tags:
+      - Continent
+    parameters:
+      - name: id
+        in: path
+        type: integer
+        required: true
+        description: ID du continent à supprimer
+    responses:
+      200:
+        description: Continent supprimé avec succès
+    """
     delete_continent(id)
     return jsonify({"message": "Continent supprimé avec succès"}), 200
 
 # Route pour mettre à jour un continent
 @bp.route('/<int:id>', methods=['PUT'])
 def update_continent_route(id):
+    """
+    Mettre à jour un continent
+    ---
+    tags:
+      - Continent
+    parameters:
+      - name: id
+        in: path
+        type: integer
+        required: true
+        description: ID du continent à mettre à jour
+      - in: body
+        name: continent
+        required: true
+        schema:
+          type: object
+          properties:
+            continent:
+              type: string
+    responses:
+      200:
+        description: Continent mis à jour avec succès
+    """
     data = request.get_json()
     continent_name = data.get('continent')
     update_continent(id, continent_name)
     return jsonify({"message": "Continent mis à jour avec succès"}), 200
 
-
-@bp.route("/index/test",  methods=['GET'])
-def index():
-    return render_template('index.html')
--- routes/daily_pandemic_country.py
+++ routes/daily_pandemic_country.py
@@ -11,6 +11,33 @@ bp = Blueprint('daily_pandemic_country', __name__, url_prefix='/daily_pandemic_c
 @bp.route('/<int:id_country>/<int:id_pandemic>', methods=['GET'])
 @bp.route('/<int:id_country>/<int:id_pandemic>/<string:date>', methods=['GET'])
 def get_daily_data_route(id_country, id_pandemic, date=None):
+    """
+    Récupérer les données journalières pour un pays et une pandémie
+    ---
+    tags:
+      - Daily Pandemic Country
+    parameters:
+      - name: id_country
+        in: path
+        required: true
+        type: integer
+      - name: id_pandemic
+        in: path
+        required: true
+        type: integer
+      - name: date
+        in: path
+        required: false
+        type: string
+        format: date
+    responses:
+      200:
+        description: Données journalières récupérées
+      404:
+        description: Aucune donnée trouvée
+      400:
+        description: Mauvais format de date
+    """
     try:
         if date:  # Si une date est fournie dans l'URL
             daily_data = get_daily_data(id_country, id_pandemic, date)
@@ -32,6 +59,33 @@ def get_daily_data_route(id_country, id_pandemic, date=None):
 # Ajouter des données journalières
 @bp.route('/', methods=['POST'])
 def add_daily_data_route():
+    """
+    Ajouter des données journalières
+    ---
+    tags:
+      - Daily Pandemic Country
+    parameters:
+      - in: body
+        name: daily_data
+        required: true
+        schema:
+          type: object
+          properties:
+            id_country:
+              type: integer
+            id_pandemic:
+              type: integer
+            date:
+              type: string
+              format: date
+            daily_new_deaths:
+              type: integer
+            daily_new_cases:
+              type: integer
+    responses:
+      201:
+        description: Données journalières ajoutées avec succès
+    """
     data = request.get_json()
     id_country = data.get('id_country')
     id_pandemic = data.get('id_pandemic')
@@ -44,6 +98,39 @@ def add_daily_data_route():
 # Mettre à jour des données journalières
 @bp.route('/<int:id_country>/<int:id_pandemic>/<string:date>', methods=['PUT'])
 def update_daily_data_route(id_country, id_pandemic, date):
+    """
+    Mettre à jour des données journalières
+    ---
+    tags:
+      - Daily Pandemic Country
+    parameters:
+      - name: id_country
+        in: path
+        required: true
+        type: integer
+      - name: id_pandemic
+        in: path
+        required: true
+        type: integer
+      - name: date
+        in: path
+        required: true
+        type: string
+        format: date
+      - in: body
+        name: daily_data
+        required: true
+        schema:
+          type: object
+          properties:
+            daily_new_deaths:
+              type: integer
+            daily_new_cases:
+              type: integer
+    responses:
+      200:
+        description: Données journalières mises à jour
+    """
     data = request.get_json()
     daily_new_deaths = data.get('daily_new_deaths')
     daily_new_cases = data.get('daily_new_cases')
@@ -53,5 +140,28 @@ def update_daily_data_route(id_country, id_pandemic, date):
 # Supprimer des données journalières
 @bp.route('/<int:id_country>/<int:id_pandemic>/<string:date>', methods=['DELETE'])
 def delete_daily_data_route(id_country, id_pandemic, date):
+    """
+    Supprimer des données journalières
+    ---
+    tags:
+      - Daily Pandemic Country
+    parameters:
+      - name: id_country
+        in: path
+        required: true
+        type: integer
+      - name: id_pandemic
+        in: path
+        required: true
+        type: integer
+      - name: date
+        in: path
+        required: true
+        type: string
+        format: date
+    responses:
+      200:
+        description: Données journalières supprimées avec succès
+    """
     delete_daily_data(id_country, id_pandemic, date)
     return jsonify({"message": "Daily data deleted successfully"})
